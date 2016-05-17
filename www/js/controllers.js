75bf0801393c3ff066841647f6ab5f22020e11de
Cleanique Coders <cleanique.coders@gmail.com>
2016-05-18 01:43:31 +0800

    initial

---
+++ www/js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('toDoAppCtrl', function($scope) {
+
+})
+   
+.controller('newTaskCtrl', function($scope) {
+
+})
+   
+.controller('editTaskCtrl', function($scope) {
+
+})
+   
+.controller('taskDetailsCtrl', function($scope) {
+
+})
+ 
